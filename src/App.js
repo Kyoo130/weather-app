937daf14505ed6df5dd82a64dca397481a2d26ab
Kyoo130 <kml9003@kakao.com>
2022-04-23 02:20:19 +0900

    :sparkles: WeatherBox, Button 컴포넌트 생성 및 API 연동

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import WeatherBox from "./components/WeatherBox";
+import WeatherButton from "./components/WeatherButton";
 
 function App() {
+  const [weather, setWeather] = useState(null);
+
+  const getCurrentLocation = () => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      let lat = position.coords.latitude;
+      let lon = position.coords.longitude;
+      getWeatherByCurrentLocation(lat, lon);
+    });
+  };
+
+  const getWeatherByCurrentLocation = async (lat, lon) => {
+    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_WEATHER_API_KEY}&units=metric`;
+    let response = await fetch(url);
+    let data = await response.json();
+    setWeather(data);
+  };
+
+  useEffect(() => {
+    getCurrentLocation();
+  }, []);
+
   return (
     <div>
-      Weather App
+      <div className="container">
+        <WeatherBox weather={weather} />
+        <WeatherButton />
+      </div>
     </div>
   );
 }
